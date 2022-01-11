87df6a28c02f85b985f4ccbc465aeb94a8850657
Sergey2019Kalinin <togiveknowledge@gmail.com>
2022-01-11 23:18:36 +0500

    hw 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
+  if(arr1.length==arr2.length && arr1.every((v,i)=> v === arr2[i])) {
+    result=true;
+  } else {
+    result=false;
+  }
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter(item => item > 0).filter(item2 => item2 % 3 === 0).map(item3 => item3 *= 10);
 
   return resultArr; // array
 }
