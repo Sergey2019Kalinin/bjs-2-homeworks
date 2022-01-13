ab58bd1ea5124fcca4cf945df6c29f6f2f22e715
Sergey2019Kalinin <togiveknowledge@gmail.com>
2022-01-14 00:13:59 +0500

    4 HW

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+};
 
-}
+let user1 = new Student("Ivanov", "male", "24");
+let user2 = new Student("Petrova", "female", "32");
+let user3 = new Student("Sidorov", "male", "17");
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+     this.marks = [];
+     this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+};
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined) { 
+    this.marks = [];
+    for(let i=0; i < marks.length; i++) {
+      this.marks.push(marks[i]); }
+    } else {
+    for(let i=0; i < marks.length; i++) {
+      this.marks.push(marks[i]); }
+    }
+};
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for(let i = 0; i < this.marks.length; i++) {
+  sum +=  this.marks[i]; }
+  let average = sum/this.marks.length;
+  return sum/this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.exclude = reason;
+}
