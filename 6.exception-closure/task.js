bb0988b80125f43c91710e2717aad5b93a548f5b
Sergey2019Kalinin <togiveknowledge@gmail.com>
2022-01-23 21:10:36 +0500

    6 HW

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,18 +1,19 @@
 //Задание 1
 
 const parseCount = (parseArg) => {
-	if (isNaN(Number.parseInt (parseArg))) {
-		 const parseError = new Error ("Невалидное значение");
-		 throw parseError;
+	let parseResult = Number.parseInt (parseArg);
+	if (isNaN (parseResult)) {
+		throw new Error ("Невалидное значение");
   }
-	return Number.parseInt (parseArg);
+	return parseResult;
 };
 
 const validateCount = (parseArg) => {
 	try { parseCount;
 		return parseCount (parseArg);
-	} catch (parseError)	{
-		return parseError;
+
+	} catch	{
+		return ("Невалидное значение");
 	}
 };
 
@@ -30,26 +31,44 @@ class Triangle {
 		}
 	};
 
+	let perimeter = this.sideA + this.sideB + this.sideC;
+
 	getPerimeter () { 
-		return this.sideA + this.sideB + this.sideC;
+		return perimeter;
 	};
 
 	getArea ()	{
-		let p = 0.5 * (this.sideA + this.sideB + this.sideC);
+		let p = 0.5 * perimeter;
 		let triangleArea = Math.pow (p * ( p - this.sideA) * ( p - this.sideB) * ( p - this.sideC),	0.5);
 
 		return +triangleArea.toFixed(3);
 	}
 };
 
+class noTriangle extends Triangle {
+	constructor (sideA, sideB, sideC) {
+		super (sideA, sideB, sideC);
+	}
+		
+	getPerimeter () { 
+		return ("Ошибка! Треугольник не существует");
+	}
+
+	getArea ()	{
+		return ("Ошибка! Треугольник не существует");
+	}
+};
+
 const getTriangle = (sideA, sideB, sideC) => {
 	try	{ customTriangle = new Triangle (sideA, sideB, sideC);
 		return customTriangle;
-	}	catch (sideSumError) { if ((sideA + sideB) < sideC || (sideA + sideC) < sideB || (sideB + sideC) < sideB) {
-	        this.getArea;
-	        this.getPerimeter;
-	        const SumError = new Error("Ошибка! Треугольник не существует");
-	        throw SumError;l
-  		}
-  }
+
+	}	catch (sideSumError) {
+			if ((sideA + sideB) < sideC || (sideA + sideC) < sideB
+			|| (sideB + sideC) < sideB) {
+
+				let noTriangleObject = new noTriangle (sideA, sideB, sideC);
+				return	noTriangleObject;
+  			}
+   		}
 };
