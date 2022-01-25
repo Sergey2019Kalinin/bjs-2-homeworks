800e7ed1dbd7a7fd667511c8e605462d90638bfd
Sergey2019Kalinin <togiveknowledge@gmail.com>
2022-01-25 22:11:49 +0500

    correct 6 HW

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,75 +1,74 @@
 //Задание 1
 
 const parseCount = (parseArg) => {
-	let parseResult = Number.parseInt (parseArg);
+  let parseResult = Number.parseInt(parseArg);
 
-	if (isNaN (parseResult)) {
-		throw new Error ("Невалидное значение");
+  if (isNaN(parseResult)) {
+    throw new Error("Невалидное значение");
   }
-	return parseResult;
+  return parseResult;
 };
 
 const validateCount = (parseArg) => {
-	try { parseCount (parseArg);
-		return parseCount (parseArg);
 
-	} catch (parseError)	{
-		return parseError;
-	}
+  try {
+    return parseCount(parseArg);
+
+  } catch (parseError) {
+    return parseError;
+  }
 };
 
 //Задание 2
 
 class Triangle {
-	constructor (sideA, sideB, sideC) {
-		this.sideA = sideA;
-		this.sideB = sideB;
-		this.sideC = sideC;
+  
+  constructor(sideA, sideB, sideC) {
+    this.sideA = sideA;
+    this.sideB = sideB;
+    this.sideC = sideC;
 
-		if ((sideA + sideB) < sideC || (sideA + sideC) < sideB
-			|| (sideB + sideC) < sideB)	{
+    if (
+      ((sideA + sideB) < sideC) ||
+      ((sideA + sideC) < sideB) ||
+      ((sideB + sideC) < sideA)
+       ) {
+      throw new Error("Треугольник с такими сторонами не существует");
+         }
+  }
 
-			throw new Error
-		("Треугольник с такими сторонами не существует");
-		}
-	};
+  getPerimeter() {
+    return (this.sideA + this.sideB + this.sideC);
+  }
 
-	
+  getArea() {
+    let p = 0.5 * this.getPerimeter();
+    let triangleArea = Math.pow(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC), 0.5);
 
-	getPerimeter () { 
-		let perimeter = this.sideA + this.sideB + this.sideC;
-		return perimeter;
-	};
-
-	getArea ()	{
-		let p = 0.5 * this.getPerimeter ();
-		let triangleArea = Math.pow
-		(p * ( p - this.sideA) * ( p - this.sideB) * ( p - this.sideC),	0.5);
-
-		return +triangleArea.toFixed(3);
-	}
+    return +triangleArea.toFixed(3);
+  }
 };
 
 const getTriangle = (sideA, sideB, sideC) => {
 
-	try	{ customTriangle = new Triangle (sideA, sideB, sideC);
-		return customTriangle;
+  try {
+    customTriangle = new Triangle(sideA, sideB, sideC);
+    return customTriangle;
 
-	}	catch (sideSumError) {
-			let noTriangleObject =	{
-										sideA,
-										sideB,
-										sideC,
+  } catch (sideSumError) {
 
-										getPerimeter () { 
-											return ("Ошибка! Треугольник не существует.");
-										},
+    return  ({
+              sideA,
+              sideB,
+              sideC,
 
-										getArea ()	{
-											return ("Ошибка! Треугольник не существует.");
-									}
-				};
+              getPerimeter() {
+                return "Ошибка! Треугольник не существует";
+              },
 
-			return	noTriangleObject;
-  		}
+              getArea() {
+                return "Ошибка! Треугольник не существует";
+              }
+            });
+    }
 };
